b8ed4982aea4dd3d6ca376e6996983b6674801de
Divyasingh001 <divyashunk2003@gmail.com>
2025-03-20 22:41:48 +0530

    Creation of constructor

--- AddressBookCode.js
+++ AddressBookCode.js
@@ -1,3 +1,4 @@
+// UC-1
 class Contact{
     firstName;
     lastName;
@@ -7,30 +8,16 @@ class Contact{
     zipCode;
     email;
     phoneNumber;
-    constructor(firstName, lastName, address, city, state, zipCode, phoneNumber, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+
+   //created constructor
+    constructor(firstName,lastName,address,state,city,zipCode,email,phoneNumber){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.address = address
+        this.state = state
+        this.city = city
+        this.zipCode = zipCode
+        this.email = email
+        this.phoneNumber = phoneNumber
     }
 }
-
-// Array to store contacts
-let addressBook = [];
-
-// Function to add a Contact to the Address Book
-function addContact(firstName, lastName, address, city, state, zipCode, phoneNumber, email) {
-    let newContact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
-    addressBook.push(newContact);
-    console.log("Contact added successfully:", newContact);
-}
-
-// Example usage
-addContact("Divya", "Singh", "Mahadeo Nagar", "Pankroshi", "Varanasi", "221007", "9876543210", "divya@example.com");
-
-// Display the Address Book
-console.log("Address Book:", addressBook);
